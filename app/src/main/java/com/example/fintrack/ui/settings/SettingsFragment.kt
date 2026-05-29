91726d33f37b7f6f0bba4ec9e909ac90475dc11d
Ridho <m.ridhozainie123@gmail.com>
2026-05-29 17:34:55 +0800

    Membuat sistem untuk menampilkan ID akun sekarang (current session)

--- app/src/main/java/com/example/fintrack/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/fintrack/ui/settings/SettingsFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.fintrack.DatabaseHelper
+import com.example.fintrack.MainActivity
 import com.example.fintrack.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
+    private lateinit var dbHelper: DatabaseHelper
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +24,23 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingsViewModel =
+        //val settingsViewModel =
             ViewModelProvider(this)[SettingsViewModel::class.java]
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        /*
         val textView: TextView = binding.textSettings
         settingsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+         */
+
+        val currentAccountId = (activity as MainActivity).currentAccountId.toString()
+        binding.tvAccountId.text = (currentAccountId)
+
         return root
     }
 
